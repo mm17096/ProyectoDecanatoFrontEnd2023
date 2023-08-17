8eecbc39e042209a32904e5638b35e1b16354904
Victor Alonso <aa14037@ues.edu.sv>
2023-08-17 09:36:38 -0600

    Solicitud vehiculo Menu

---
+++ src/app/modules/solicitud-vehiculo/pages/mis-solicitudes/mis-solicitudes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mis-solicitudes',
+  templateUrl: './mis-solicitudes.component.html',
+  styleUrls: ['./mis-solicitudes.component.scss']
+})
+export class MisSolicitudesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

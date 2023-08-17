b97106edb92a87278a1fd5047a92fb9656726f01
Victor Alonso <aa14037@ues.edu.sv>
2023-08-17 09:35:56 -0600

    Solicitud vehiculo Menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,7 @@ const routes: Routes = [
   // tslint:disable-next-line: max-line-length
   { path: '', component: LayoutComponent, loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule), canActivate: [AuthGuard] },
   { path: 'pages', loadChildren: () => import('./extrapages/extrapages.module').then(m => m.ExtrapagesModule), canActivate: [AuthGuard] },
+  { path: 'solicitud-vehiculo', component: LayoutComponent, loadChildren: () => import('./modules/solicitud-vehiculo/solicitud-vehiculo.module').then(m => m.SolicitudVehiculoModule), canActivate: [AuthGuard] },
   { path: 'crypto-ico-landing', component: CyptolandingComponent },
 
   { path: '**', component: Page404Component },

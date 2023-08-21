9351358f3457293060b3bca0a4df4b1dda9c8481
Victor Alonso <aa14037@ues.edu.sv>
2023-08-21 08:56:11 -0600

    modal

--- src/app/modules/solicitud-vehiculo/shared/modal/modal.component.ts
+++ src/app/modules/solicitud-vehiculo/shared/modal/modal.component.ts
@@ -12,6 +12,7 @@ export class ModalComponent implements OnInit {
 
   @Input() leyenda!: string;
   @Input() titulo!: string;
+  fechaDeHoy: string = this.obtenerFechaActual(new Date());
 
   formularioSoliVe!: FormGroup;
   constructor(private modalService: NgbModal, private fb: FormBuilder, private router: Router) { }
@@ -22,11 +23,32 @@ export class ModalComponent implements OnInit {
 
   iniciarFormulario(){
     this.formularioSoliVe = this.fb.group({
-      fechaSolicitud: ['', [Validators.required]],
+      fechaSolicitud: [this.obtenerFechaActual(new Date()), [Validators.required]],
       fechaSalida: ['', [Validators.required]],
+      unidadSolicitante: ['', [Validators.required]],
+      tipoVehiculo: ['', [Validators.required]],
+      vehiculo: ['', [Validators.required]],
+      objetivoMision: ['', [Validators.required]],
+      lugarMision: ['', [Validators.required]],
+      depto: ['', [Validators.required]],
+      municipio: ['', [Validators.required]],
+      distrito: ['', []],
+      canton: ['', [Validators.required]],
+      horaSalida: ['', [Validators.required]],
+      horaRegreso: ['', [Validators.required]],
+      cantidadPersonas: [1, [Validators.required, Validators.min(1)]],
     });
   }
   openModal(content: any) {
-    this.modalService.open(content, {size: 'lg'});
+    this.modalService.open(content, {size: 'lg', backdrop: 'static'});
+  }
+
+  obtenerFechaActual(date: Date): string {
+    const year = date.getFullYear();
+    const mes = (date.getMonth() + 1).toString().
+      padStart(2, '0');
+    const dia = date.getDate().toString().
+      padStart(2, '0');
+    return `${year}-${mes}-${dia}`;
   }
 }

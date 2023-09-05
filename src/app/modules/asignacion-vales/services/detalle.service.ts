5e67049e6cf6757a79c7fffa90487b46b72d1abe
Absalon2327 <cm16057@ues.edu.sv>
2023-09-05 10:14:11 -0600

    asignaciones de vales interfaz

--- src/app/modules/asignacion-vales/services/detalle.service.ts
+++ src/app/modules/asignacion-vales/services/detalle.service.ts
@@ -1,12 +1,19 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { IAsignacionDetalle } from '../interfaces/asignacion.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class DetalleService {
-  getDetalleAsignacionValePage() {
-    throw new Error('Method not implemented.');
-  }
 
-  constructor() { }
+
+
+
+  url = "http://localhost:8080/asignacionvale/listar/cc0a3fa0-984e-4d10-9c11-deded04a3dae";
+  constructor(private http: HttpClient) {}
+
+  getDetalleAsignacionVale() {
+    return this.http.get<IAsignacionDetalle>(this.url);
+  }
 }

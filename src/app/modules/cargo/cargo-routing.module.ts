4a590fa8a7d894137193d059669de742c2e84b51
walterEDR <dr13018@ues.edu.sv>
2023-09-21 12:07:48 -0600

    agregando role en el usuarioService

--- src/app/modules/cargo/cargo-routing.module.ts
+++ src/app/modules/cargo/cargo-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { ListarComponent } from './pages/listar/listar.component';
 import { AuthSecretariaGuard } from 'src/app/core/guards/auth-secretaria.guard';
 
-const routes: Routes = [ { path: 'listar', component: ListarComponent,canActivate:[AuthSecretariaGuard] }];
+const routes: Routes = [ { path: 'listar', component: ListarComponent}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],

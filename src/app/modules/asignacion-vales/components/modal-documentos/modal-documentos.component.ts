a90609e4ea8ee3e76a1d4ad1c38c5558c33b0fbf
Absalon2327 <cm16057@ues.edu.sv>
2023-10-01 14:38:13 -0600

    fix bugs, estandar de confimaciones en solicitudes

--- src/app/modules/asignacion-vales/components/modal-documentos/modal-documentos.component.ts
+++ src/app/modules/asignacion-vales/components/modal-documentos/modal-documentos.component.ts
@@ -41,11 +41,8 @@ export class ModalDocumentosComponent implements OnInit {
 
   ngOnInit(): void {
     this.formBuilder = this.Iniciarformulario();
-    this.ObtenerSolicitudValeById(this.codigoAsignacion);
-    this.obtenerLista(this.idSolicitud);
-    this.obtenerSolicitud(this.idSolicitud);
+   // this.ObtenerSolicitudValeById(this.codigoAsignacion);
   }
-
 
   guardar() {
     if (this.formBuilder.valid) {
@@ -65,15 +62,14 @@ export class ModalDocumentosComponent implements OnInit {
     }
   }
 
-  ObtenerSolicitudValeById(codigoA: string) {
+  ObtenerSolicitudValeById(codigoA: string, content: any) {
     this.detalleservice.getAsignacionValeSolicitudVale(codigoA).subscribe({
       next: (data) => {
         this.asignacionSolicitud = data;
         this.idSolicitud =
           this.asignacionSolicitud.solicitudVale.idSolicitudVale;
-          console.log("solicitud: ",this.idSolicitud);
-          this.obtenerLista(this.idSolicitud);
-          this.obtenerSolicitud(this.idSolicitud);
+        console.log("solicitud: ", this.idSolicitud);
+        this.obtenerSolicitud(this.idSolicitud, content);
       },
     });
   }
@@ -187,10 +183,34 @@ export class ModalDocumentosComponent implements OnInit {
   }
   openModal(content: any) {
 
-    console.log("tamaño: ",this.obtenerLista(this.idSolicitud));
+    console.log("codigo A: ", this.codigoAsignacion);
+
+    this.ObtenerSolicitudValeById(this.codigoAsignacion, content);
+
+    this.obtenerLista(this.idSolicitud, content);
+    //this.obtenerLista(this.idSolicitud, content);
+    //this.obtenerSolicitud(this.idSolicitud, content);
+    /* console.log("tamaño: ", );
+
     if (this.estadoEntrada == 2) {
+      if (this.sizeDocs == 2) {
+        this.mensajesService.mensajesToast(
+          "info",
+          "Ya se registraron todos los documentos"
+        );
+      } else {
+        this.modalService.open(content, { size: "lx", centered: true });
+      }
+    } else {
+      this.mensajesService.mensajesToast(
+        "warning",
+        "Vehículo no ha regresado de la misión"
+      );
+    } */
+  }
 
-
+  validaciones(content: any) {
+    if (this.estadoEntrada == 2) {
       if (this.sizeDocs == 2) {
         this.mensajesService.mensajesToast(
           "info",
@@ -248,14 +268,18 @@ export class ModalDocumentosComponent implements OnInit {
   get Listamisiones() {
     return this.detalleservice.listDeMisiones;
   }
-  obtenerSolicitud(id: string) {
+  obtenerSolicitud(id: string, content: any) {
+
     this.detalleservice.getSolicitudVale(id).subscribe({
       next: (data) => {
         this.estadoEntrada = data[0].estadoEntradaSolicitudVale;
+        this.validaciones(content);
       },
     });
   }
-  obtenerLista(id: string) {
+  obtenerLista(id: string, content: any) {
+    console.log("id: ", id);
+
     //para poder mostrar e la tabla
     this.detalleservice.ObtenerLista(id).subscribe(
       (resp: IDocumentosvale[]) => {
@@ -263,7 +287,7 @@ export class ModalDocumentosComponent implements OnInit {
         console.log("lista: ", resp);
         console.log("tamaño de la lista: ", this.sizeDocs);
         this.sizeDocs = resp.length;
-
+        this.validaciones(content);
       },
       (error) => {
         // Manejar errores aquí
